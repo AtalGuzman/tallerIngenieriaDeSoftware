13ce315e472a0c15b31f467d579ec04fa8a7ffea
Adolfo Guzmán <Adolfo Guzmn>
2017-04-25 22:53:06 -0300

    Se agrega la creación del documento de orden de trabajo, SIN FORMATO

--- www/controllers/newWorkOrderCtrl.js
+++ www/controllers/newWorkOrderCtrl.js
@@ -53,7 +53,96 @@ angular.module('starter').controller('newWorkOrderCtrl', function($scope, $state
     for( var v in $scope.data){
       console.log($scope.data[v]);
     }
-  };
 
+    var doc = {
+      content: [
+        {text:[$scope.data.tipo_documento,"\n\n",$scope.data.folio],style:'header'},
+        {
+          table: {
+            style:'tableExample',
+            widths: ['*'],
+            body:[
+              [{text: "\n\nDATOS GENERALES\n\n",style:'subheader'}],
+              [
+                {
+                  alignment: 'justify',
+                  text: [
+                  "PROYECTO         :\t",$scope.data.datos_generales.proyecto,"\n",
+                  "PROPIEDAD        :\t",$scope.data.datos_generales.propiedad,"\n",
+                  "TIPO DE PROPIEDAD:\t",$scope.data.datos_generales.tipo_de_propiedad,"\n",
+                  "FECHA R.M.       :\t",$scope.data.datos_generales.fecha_rm,"\n",
+                  "PROPIETARIO      :\t", $scope.data.datos_generales.propietario,"\n",
+                  "E-MAIL           :\t",$scope.data.datos_generales.email,"\n",
+                  "TELEFONOS        :\t",$scope.data.datos_generales.telefonos,"\n",
+                  "ETAPA            :\t",$scope.data.datos_generales.etapa,"\n",
+                  "MANZANA          :\t",$scope.data.datos_generales.manzana_lote,"\n",
+                  "FECHA ENTREGA    :\t",$scope.data.datos_generales.fecha_entrega,"\n"
+                  ]
+                }
+              ]
+            ]
+          } 
+        },
+        {text:"\n\n"},
+        {
+          table: {
+            style:'tableExample',
+            widths: ['*'],
+            body:[
+              [{text: "\n\nDATOS SOLICITUD\n\n",style:'subheader'}],
+              [
+                {
+                  alignment: 'justify',
+                  text: [
+                      "FECHA                :\t",$scope.data.datos_solicitud.fecha_solicitud,"\n",
+                      "VISITA EFECTUADA POR :\t",$scope.data.datos_solicitud.medio_solicitud,"\n",
+                      "MEDIO SOLICITUD      :\t",$scope.data.datos_solicitud.visita_efectuada_por,"\n",
+                      "NOMBRE QUIEN RECIBE  :\t",$scope.data.datos_solicitud.nombre_quien_recibe,"\n"
+                    ]
+                }
+              ]
+            ]
+          }
+        },
+        {text:"\n\n"},  
+        {
+          table: {
+            style:'tableExample',
+            widths: ['*'],
+            body:[
+              [{text: "\n\nDATOS TRABAJO A REALIZAR\n\n",style:'subheader'}],
+              [
+                {
+                  alignment: 'justify',
+                  text: [
+                      "DURACION ESTIMADA      :\t",$scope.data.datos_trabajo.duracion_estimada,"\n",
+                      "RESPONSABLE            :\t",$scope.data.datos_trabajo.responsable,"\n",
+                      "FECHA EJECUCION TRABAJO:\t",$scope.data.datos_trabajo.fecha_ejecucion,"\n",
+                      "HORA APROXIMADA        :\t",$scope.data.datos_trabajo.hora_ejecucion,"\n"
+                    ]
+                }
+              ]
+            ]
+          }
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          alignment: 'center',
+        },
+        subheader: {
+          fontSize: 15,
+          bold: true,
+          alignment: 'center'
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15]
+        }
+      }     
+    };
+    pdfMake.createPdf(doc).download($scope.var);
+  };
 
 })
